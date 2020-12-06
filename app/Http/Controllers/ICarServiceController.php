fbfb1b1d900cc653d8a60d67c377f4828852c956
Stefan <stefanvoid89@gmail.com>
2020-12-06 14:01:21 +0100

    adding validation in getIdent

--- app/Http/Controllers/ICarServiceController.php
+++ app/Http/Controllers/ICarServiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class ICarServiceController extends Controller
 {
@@ -32,8 +33,18 @@ class ICarServiceController extends Controller
 
     public function getIdent(Request $request)
     {
-        $input = (object) json_decode($request->getContent(), true);
-        $idents =  collect(DB::select("SELECT ltrim(rtrim(cast(ta.NumInterno as char))) as idArtikla, ta.Referencia as katBroj, ta.Descrip as opis,isnull(taa.Ubicacion1,'') as lokacija,
+
+        $validator = Validator::make($request->all(), [
+            'dokument' => 'required|integer',
+            'ident' => 'required',
+            'magacin' => 'required|integer',
+        ]);
+        if ($validator->fails()) {
+            // return response()->json(["errors" => implode(" ", $validator->errors()->all())], 422);
+            return response()->json(implode(" ", $validator->errors()->all()), 422);
+        }
+        $idents =  collect(DB::select(
+            "SELECT ltrim(rtrim(cast(ta.NumInterno as char))) as idArtikla, ta.Referencia as katBroj, ta.Descrip as opis,isnull(taa.Ubicacion1,'') as lokacija,
         ltrim(rtrim(cast(taa.CdadStock as char))) as stanjeKnjige, ltrim(rtrim(cast(isnull(q.unesenaKolicina,0) as char))) as unesenaKolicina , 
         ltrim(rtrim(cast(taa.CdadStock  - isnull(q.unesenaKolicina,0) as char)))  as novaKolicina
         FROM taArticulo ta 
@@ -41,8 +52,9 @@ class ICarServiceController extends Controller
         outer apply (select idArtikla, sum(Stanje) as unesenaKolicina from Popis2012Test p where 1=1 and p.IdArtikla = ta.NumInterno 
         and Dokument = ?	 group by IdArtikla)q
         WHERE ta.Referencia LIKE RTRIM(?)+'%' AND taa.Almacen = cast(? as int)  AND taa.Emp = '001' 
-        ORDER BY len(ta.Referencia),lokacija desc, ta.Referencia,CdadStock DESC, ta.Marca DESC", [$input->dokument, $input->ident, $input->magacin]));
-        //  return response()->json(json_decode($request->getContent(), true));
+        ORDER BY len(ta.Referencia),lokacija desc, ta.Referencia,CdadStock DESC, ta.Marca DESC",
+            [$request->input('dokument'), $request->input('ident'), $request->input('magacin')]
+        ));
         return response()->json($idents);
     }
 }
